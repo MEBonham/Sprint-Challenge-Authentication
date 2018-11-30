6b2f95afa94fde8374ec5eed980cd04e1576b342
MEBonham <programmer@mckaybonham.com>
2018-11-30 11:22:04 -0700

    Finished MVP, used create-react-app

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,13 @@
 const axios = require('axios');
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
+const jwtKey = require('../_secrets/keys').jwtKey;
+
+const db = require("../database/dbConfig.js");
 const { authenticate } = require('./middlewares');
+
+const rounds = 14;
 
 module.exports = server => {
   server.post('/api/register', register);
@@ -8,18 +15,60 @@ module.exports = server => {
   server.get('/api/jokes', authenticate, getJokes);
 };
 
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username
+  };
+  const secret = jwtKey;
+  const options = {
+    expiresIn: "1h"
+  };
+
+  return jwt.sign(payload, secret, options);
+}
+
 function register(req, res) {
   // implement user registration
+  const creds = req.body;
+  const hash = bcrypt.hashSync(creds.password, rounds);
+  creds.password = hash;
+
+  db("users")
+    .insert(creds)
+    .then(ids => {
+      res.status(201).json(ids);
+    })
+    .catch(err => {
+      res.status(500).json({ error: err });
+    });
 }
 
 function login(req, res) {
   // implement user login
+  const creds = req.body;
+
+  db("users")
+    .where({ username: creds.username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(creds.password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({ message: "Welcome", token });
+      } else {
+        res.status(401).json({ message: "Incorrect username or password" });
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 }
 
 function getJokes(req, res) {
   axios
     .get(
-      'https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_ten'
+      // 'https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_ten'
+      "https://safe-falls-22549.herokuapp.com/random_ten"
     )
     .then(response => {
       res.status(200).json(response.data);
